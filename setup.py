60e7d829493bf874270359ee8aeb692509657b56
Daniel Kastinen <daniel.kastinen@irf.se>
2022-02-05 11:29:08 +0100

    package setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+import pathlib
+import codecs
+
+HERE = pathlib.Path(__file__).resolve().parents[0]
+
+def get_version(path):
+    with codecs.open(path, 'r') as fp:
+        for line in fp.read().splitlines():
+            if line.startswith('__version__'):
+                delim = '"' if '"' in line else "'"
+                return line.split(delim)[1]
+        else:
+            raise RuntimeError("Unable to find version string.")
+
+setuptools.setup(
+    version=get_version(HERE / 'src' / 'spycy_invaders' / 'version.py'),
+)
